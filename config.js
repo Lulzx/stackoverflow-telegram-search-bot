54b85c9847f5573e43d80cec7520095d29fc65fa
Lulzx <wiserishabh@gmail.com>
2017-09-04 11:39:51 +0530

    Update and rename config.js.example to config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+ 'TelegramToken': '395278393:AAHk_92Xret3YICav1ewjppWv-wxNZPTnJc', // Telegram token
+ 'SOKey': "10713", // Stackexchange api token
+ 'SOSecret': "DywPsuKRev2iEk1hLtfzRA((" // Stackexchange api secret
+}
